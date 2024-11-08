facdffef12918db72f6aa16619c5e736d1784a38
RahullSainii <142921451+RahullSainii@users.noreply.github.com>
2024-11-09 00:28:32 +0530

    Time: 7 ms (34.29%), Space: 95.2 MB (99.68%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -8,11 +8,12 @@ public:
         for(int i=0;i<n;i++){
             XOR^=nums[i];
         }
+        //to find flipmfirst find mask having all bits set to 1
         int mask((1<<maximumBit)-1);
         for(int i=0;i<n;i++){
-            int k=XOR^mask;
+            int k=XOR^mask;//this will give flipped value of xor 
             result[i]=k;
-            XOR=(XOR^nums[n-1-i]);
+            XOR=(XOR^nums[n-1-i]);//rightmost values ka XOR niklega only jonse index pai honge
         }
         return result;
     }
