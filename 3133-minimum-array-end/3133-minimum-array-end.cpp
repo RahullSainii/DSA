e7b76771972fcf2e88ab19fc1ca65e9b9b00eb6b
RahullSainii <142921451+RahullSainii@users.noreply.github.com>
2024-11-10 04:02:26 +0530

    Time: 1745 ms (6.56%), Space: 8.3 MB (92.73%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -3,7 +3,7 @@ public:
     long long minEnd(int n, int x) {
         long long num=x;
         for(int i=1;i<n;i++){
-            num=(num+1) | x;
+            num=(num+1)|x;
         }
         return num;
     }
