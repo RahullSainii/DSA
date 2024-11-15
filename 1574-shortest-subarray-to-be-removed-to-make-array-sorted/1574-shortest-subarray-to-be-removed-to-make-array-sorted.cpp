7235638d9629a3e4256f83680ad74528ce3605d3
RahullSainii <142921451+RahullSainii@users.noreply.github.com>
2024-11-16 03:08:44 +0530

    Time: 0 ms (100.00%), Space: 69.6 MB (25.64%) - LeetHub

---
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n=arr.size();
+        //find jth pointer from right side
+        int j=n-1;
+        while(j>0 && arr[j]>=arr[j-1]){
+            j--;
+        }
+        int i=0;
+        int result=j;
+        //step 2: start finding correct i and j elements and find deleted j-i-1
+        while(i<j && (i==0|| arr[i]>=arr[i-1])){
+            while(j<n && arr[i]>arr[j]){
+                j++;
+            }
+            result=min(result,j-i-1);
+            i++;
+        }
+        return result;
+    }
+};
