3a3be42bff909a97f92d2ea9771a0d49854167c3
RAHUL SAINI <142921451+RahullSainii@users.noreply.github.com>
2026-09-18 23:28:18 +0530

    1801-AVERAGE-TIME-OF-PROCESS-PER-MACHINE Stats: Time: 319 ms (19.64%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,11 +1,10 @@
-SELECT machine_id,
-       ROUND(AVG(end_time - start_time), 3) AS processing_time
-FROM (
-    SELECT machine_id,
-           process_id,
-           MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time,
-           MAX(CASE WHEN activity_type = 'end'   THEN timestamp END) AS end_time
-    FROM Activity
-    GROUP BY machine_id, process_id
-) AS t
-GROUP BY machine_id;
+SELECT 
+    s.machine_id,
+    ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+FROM Activity s
+JOIN Activity e
+    ON s.machine_id = e.machine_id
+    AND s.process_id = e.process_id
+    AND s.activity_type = 'start'
+    AND e.activity_type = 'end'
+GROUP BY s.machine_id;
